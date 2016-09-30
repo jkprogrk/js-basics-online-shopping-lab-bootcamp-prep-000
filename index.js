21457eda10fe949aeda507d055e0548b3a270874
Jonathan Kemp <jonathan_kemp@talk21.com>
2016-09-30 15:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,27 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.random() * 100;
+  Math.floor(price);
+  cart.push({item: price})
+  console.log(`${item} has been added to your cart.`)
+  return cart;
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  var cartItem = Object.keys(cart);
+  for (var i = 0, l = cartItem.length; i < l; i++) {
+    console.log(`In your cart, you have socks at $${cart[cartItem[i]]}, puppy at $${cart[cartItem[i]]}, iPhone at $${cart[cartItem[i]]}. `)
+  }
+
 }
